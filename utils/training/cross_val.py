e5af062f29a95df62afac2b017079eb9a1f14c48
zhack47 <mesbah.zacharia@sfr.fr>
2025-07-07 13:58:28 +0200

    Debug: Feature selection

--- utils/training/cross_val.py
+++ utils/training/cross_val.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sksurv.svm.survival_svm import FastSurvivalSVM
+from sksurv.linear_model.coxnet import CoxnetSurvivalAnalysis
 from sksurv. util import Surv
 from sksurv.metrics import concordance_index_censored
 
@@ -10,6 +11,10 @@ if __name__ == "__main__":
     endpoints_path = sys.argv[2]
     organomics = pd.read_csv(organomics_path)
     endpoints = pd.read_csv(endpoints_path)
+
+    # Remove samples with no endpoint
+    organomics = organomics[organomics["Patient_ID"].isin(endpoints["PatientID"])]
+
     patient_ids = organomics["Patient_ID"]
     train_ids = patient_ids[:round(len(patient_ids)*.8)]
     test_ids = patient_ids[round(len(patient_ids)*.8):]
@@ -24,9 +29,12 @@ if __name__ == "__main__":
     X_train = X_train.fillna(0)
     X_test = X_test.fillna(0)
     del X_train["Patient_ID"]
+    del X_test["Patient_ID"]
+    for col in X_train.columns:
+        model = CoxnetSurvivalAnalysis()
+        model.fit(X_train[col], Y_train)
+        corr_score = concordance_index_censored(Y_train["event"], Y_train["time"], model.predict(X_train[col]))
     model = FastSurvivalSVM()
     model.fit(X_train, Y_train)
     y_hat_test = model.predict(X_test)
-    concordance_index_censored(endpoints[endpoints["PatientID"].isin(test_ids)]["Relapse"],
-                               endpoints[endpoints["PatientID"].isin(test_ids)]["RFS"],
-                               y_hat_test)
+    ci = concordance_index_censored(Y_test["event"], Y_test["time"], y_hat_test)
