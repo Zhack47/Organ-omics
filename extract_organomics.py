1852da5e6db52a20fdecfe3bd9d98b0beb339e78
Zhack <mesbah.zacharia@sfr.fr>
2025-10-16 15:25:45 +0200

    Add: json file selection

--- extract_organomics.py
+++ extract_organomics.py
@@ -3,14 +3,17 @@
 if __name__ == "__main__":
     import sys
     from argparse import ArgumentParser
-    from utils.organomics_extraction import extract_organomics
+    from utils.radiomics import extract_radiomics
 
 
     arg_parser = ArgumentParser()
     arg_parser.add_argument("-d", type=str, required=True)
     arg_parser.add_argument("-o", type=str, required=True)
+    arg_parser.add_argument("--json-file-name", type=str, required=False)
 
     arguments = arg_parser.parse_args(sys.argv[1:])
     print(arguments.d)
     print(arguments.o)
-    extract_organomics(arguments.d, arguments.o)
+    print(arguments)
+    exit()
+    extract_radiomics(arguments.d, arguments.o, arguments.json)
