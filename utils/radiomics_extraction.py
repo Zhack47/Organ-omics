5a0df576cc27e9582653bef7552faf1996068e22
Zhack <mesbah.zacharia@sfr.fr>
2025-10-16 15:42:48 +0200

    Add: json file selection

--- utils/radiomics_extraction.py
+++ utils/radiomics_extraction.py
@@ -42,11 +42,10 @@ def extract_radiomics(root_dataset_path, output_filename, json_file_name):
     _, _, names, channels, _, _, classes, spacing = load_names(root_dataset_path, json_file_name=json_file_name)
     display_radiomics_config(names, channels, classes, spacing)
     del classes["background"]  # Remove the background from channels
-    print(output_filename.split(os.sep)[:-1])
-    print(join(*output_filename.split(os.sep)[:-1]))
-    os.makedirs(join(*output_filename.split(os.sep)[:-1]), exist_ok=True)
-    out_csv_file = open(output_filename, "w", encoding="utf-8")
 
+    os.makedirs(join(*output_filename.split(os.sep)[:-1]), exist_ok=True)    
+    out_csv_file = open(output_filename, "w", encoding="utf-8")
+    print(f"Results will be saved in {output_filename}")
 
     # Write columns headers
     # We do a first blank pass in order to write the columns' names
