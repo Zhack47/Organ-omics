fa2f8fff7d3f920c8362cfb6736febf42b329a6e
Zhack <mesbah.zacharia@sfr.fr>
2025-10-16 15:42:56 +0200

    Add: json file selection

--- extract_radiomics.py
+++ extract_radiomics.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument("-d", type=str, required=True)
     arg_parser.add_argument("-o", type=str, required=True)
+    arg_parser.add_argument("--json-file-name", type=str, required=False, default="dataset.json")
 
     arguments = arg_parser.parse_args(sys.argv[1:])
     print(arguments.d)
     print(arguments.o)
-    extract_radiomics(arguments.d, arguments.o)
+    extract_radiomics(arguments.d, arguments.o, arguments.json_file_name)
