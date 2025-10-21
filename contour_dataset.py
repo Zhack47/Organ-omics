06cad3658c69b18b279a988c011ce5f2da908d02
Zhack <mesbah.zacharia@sfr.fr>
2025-10-21 13:22:11 +0200

    Add: paper example

--- contour_dataset.py
+++ contour_dataset.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     import sys
+    from os.path import join
     from argparse import ArgumentParser
     from utils.organ_contours import segment_dataset
 
@@ -12,6 +13,5 @@ if __name__ == "__main__":
     arg_parser.add_argument("--json-file-name", type=str, required=False, default="dataset.json")
 
     arguments = arg_parser.parse_args(sys.argv[1:])
-    print(arguments.d)
-    print(arguments.o)
+    print(f"Contouring images from {join(arguments.d, 'imagesTr')} and putting the results in {join(arguments.o, 'labelsTr')}")
     segment_dataset(arguments.d, arguments.o, arguments.json_file_name)
---
+++ examples/paper_example.sh
@@ -0,0 +1,2 @@
+ #!/bin/bash
+ python3 ../contour_dataset.py -d ../data/Dataset001_Test/ -o ../data/Dataset001_Test/
